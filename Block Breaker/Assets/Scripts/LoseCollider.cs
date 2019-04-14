14882ea0a525bea3ac3c204ee8d5f7b997a0f99b
Char-Mander <laurajf29@gmail.com>
2019-04-14 16:28:46 +0200

    Elements, bounce effect, some colliders, and movement for the paddle added to the project

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Loads the "Game Over" scene when the ball falls
+        SceneManager.LoadScene("Game Over"); 
+    }
+
+}
